289d183813b5e0c9e12af37de3ba9cc57bdd25fd
Aaron <Aaron@192.168.172.200>
2017-03-16 16:36:11 -0400

    Preliminary implementation of the login servlet.

--- src/edu/ycp/cs320/aroby/servlet/ajax/LoginAjaxServlet.java
+++ src/edu/ycp/cs320/aroby/servlet/ajax/LoginAjaxServlet.java
@@ -1,13 +1,18 @@
 package edu.ycp.cs320.aroby.servlet.ajax;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ycp.cs320.aroby.controller.LoginController;
 import edu.ycp.cs320.aroby.controller.NumbersController;
+import edu.ycp.cs320.aroby.model.Account;
 import edu.ycp.cs320.aroby.model.Numbers;
 
 public class LoginAjaxServlet extends HttpServlet {
@@ -26,7 +31,27 @@ public class LoginAjaxServlet extends HttpServlet {
 	}
 
 	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Account model = new Account();
+		LoginController controller= new LoginController();
+		controller.setModel(model);
+		try {
+			String inputEmail = req.getParameter("email");
+			String inputPass = req.getParameter("password");
+
+			if(inputEmail.equals("email")) {
+				model.setEmail(inputEmail);
+			}
+			if(inputPass.equals("password")) {
+				model.setPassword(inputPass);
+			}
+			else {
+				throw new Exception();
+			}
+		} catch (Exception ex) {
+			badRequest("Invalid login credentials.", resp);
+		}
 		
+		// TODO: check database to see if this matches any accounts we have
 	}
 
 	private void badRequest(String message, HttpServletResponse resp) throws IOException {
--- war/_view/login.jsp
+++ war/_view/login.jsp
@@ -25,11 +25,11 @@
 			<table>
 				<tr>
 					<td class="label">Email:</td>
-					<td><input type="text" name="first" size="12" value="${model.email}" /></td>
+					<td><input type="text" name="email" size="12" value="${email}" /></td>
 				</tr>
 				<tr>
 					<td class="label">Password:</td>
-					<td><input type="text" name="second" size="12" value="${model.password}" /></td>
+					<td><input type="password" name="password" size="12" value="${password}" /></td>
 				</tr>
 			</table>
 			<input type="Submit" name="submit" value="Login">

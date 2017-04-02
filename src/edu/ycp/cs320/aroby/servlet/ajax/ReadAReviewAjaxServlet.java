69d951a47476e203db5b32bcf0fb2b498deaa197
terrell clark <tclark22@ycp.edu>
2017-04-02 17:02:27 -0400

    tclark ms2

--- src/edu/ycp/cs320/aroby/servlet/ajax/ReadAReviewAjaxServlet.java
+++ src/edu/ycp/cs320/aroby/servlet/ajax/ReadAReviewAjaxServlet.java
@@ -30,23 +30,28 @@ public class ReadAReviewAjaxServlet extends HttpServlet {
 	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String reviewText = getString(req, "reviewText");
 		String topic = getString(req, "topic");
-		String recommendation = getString(req, "recommendation");
-		Double rating = getDouble(req, "rating");
+		String name = getString(req, "name");
+		String author = getString(req, "author");
+		String descript = getString(req, "descript");
+		String link = getString(req, "link");
 		
-		ReadAReview model = new ReadAReview();
+		ReadAReview model = new ReadAReview(name, author, topic, descript, reviewText, link);
 		model.setReviewText(reviewText);
 		model.setTopic(topic);
-		model.setRecommendation(recommendation);
-		model.setRating(rating);
+		model.setName(name);
+		model.setAuthor(author);
+		model.setDescription(descript);
+		model.setLink(link);
 		ReadAReviewController controller = new ReadAReviewController();
 		controller.setModel(model);
 		
 		resp.setContentType("text/plain");
 		resp.getWriter().println(reviewText);
 		resp.getWriter().println(topic);
-		resp.getWriter().println(rating);
-		resp.getWriter().println(recommendation);
-	
+		resp.getWriter().println(name);
+		resp.getWriter().println(author);
+		resp.getWriter().println(descript);
+		resp.getWriter().println(link);
 	}
 
 	private Double getDouble(HttpServletRequest req, String name) {

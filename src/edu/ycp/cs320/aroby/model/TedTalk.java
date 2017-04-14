6f1e399c1ea17161c754a91703cce0dc7f7a9d7f
Aaron Roby <aroby@ycp.edu>
2017-04-14 17:23:14 -0400

    Fixed some variable naming conventions with the TED Talk class

--- src/edu/ycp/cs320/aroby/model/TedTalk.java
+++ src/edu/ycp/cs320/aroby/model/TedTalk.java
@@ -5,30 +5,30 @@ import java.util.ArrayList;
 
 public class TedTalk {
 	private URL link;
-	private String title, author, topic, descript;
+	private String title, speaker, topic, description;
 	private ArrayList<Review> review = new ArrayList<Review>();
 	
-	public void setTedTalk(ArrayList<Review> review, String title, String author, String topic, String descript, URL link){
+	public void setTedTalk(ArrayList<Review> review, String title, String speaker, String topic, String descript, URL link){
 		this.review = review;
 		this.title = title;
-		this.author = author;
+		this.speaker = speaker;
 		this.topic = topic;
-		this.descript = descript;
+		this.description = descript;
 		this.link = link;
 	}
 	
 	public Review getReview(int index){
-		Review review1 = review.get(index);
-		return review1;
+		Review review = this.review.get(index);
+		return review;
 	}
 	public String getTitle(){
 		return title;
 	}
-	public String getAuthor(){
-		return author;
+	public String getSpeaker(){
+		return speaker;
 	}
 	public String getDescript(){
-		return descript;
+		return description;
 	}
 	public URL getLink(){
 		return link;

9b8638c13cce48ce07a948f3cd62becfee33943b
Chihea Locke <clocke3@ycp.edu>
2017-04-14 20:43:39 -0400

    //changed files

---
+++ src/edu/ycp/cs320/aroby/controller/TedTalkController.java
@@ -0,0 +1,16 @@
+//come back to this later 
+
+package edu.ycp.cs320.aroby.controller;
+
+import edu.ycp.cs320.aroby.model.Review;
+import edu.ycp.cs320.aroby.controller.ReviewController;
+import edu.ycp.cs320.aroby.model.TedTalk;
+
+public class TedTalkController {
+
+	private TedTalk ted_talk;
+	
+	public void set_TedTalk(){
+		
+	}
+}
